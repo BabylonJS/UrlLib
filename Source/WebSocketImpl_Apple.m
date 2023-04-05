b80c91268c515111f9167bd8ef300ee3bca53586
Liyaan Maskati <liyaanmaskati@gmail.com>
2023-04-05 09:57:23 -0700

    fix formatting and remove logs

--- Source/WebSocketImpl_Apple.m
+++ Source/WebSocketImpl_Apple.m
@@ -1,6 +1,7 @@
 #import <WebSocketImpl_Apple.h>
 
-@interface WebSocket_Impl() <NSURLSessionWebSocketDelegate> {
+@interface WebSocket_Impl() <NSURLSessionWebSocketDelegate>
+{
     NSURLSessionWebSocketTask *webSocketTask API_AVAILABLE(ios(13.0));
     NSURLSession *session;
     // store callbacks here
@@ -13,57 +14,66 @@
 
 @implementation WebSocket_Impl
 
--(void) open:(NSString *)url on_open:(void (^)(void))on_open on_close:(void (^)(void))on_close on_message:(void (^)(NSString *))on_message on_error:(void (^)(void))on_error {
+-(void) open:(NSString *)url on_open:(void (^)(void))on_open on_close:(void (^)(void))on_close on_message:(void (^)(NSString *))on_message on_error:(void (^)(void))on_error
+{
     session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:[NSOperationQueue mainQueue]];
     open_callback = on_open;
     close_callback = on_close;
     error_callback = on_error;
     message_callback = on_message;
     
-    webSocketTask = [session webSocketTaskWithURL:[NSURL URLWithString:url] ];
+    webSocketTask = [session webSocketTaskWithURL:[NSURL URLWithString:url]];
     [webSocketTask resume];
 }
 
-- (void) close {
+- (void) close 
+{
     [webSocketTask cancel];
     [self invalidateAndCancel];
 }
 
-- (void) sendMessage:(NSString *)message {
-    [webSocketTask sendMessage: [[NSURLSessionWebSocketMessage alloc] initWithString:message] completionHandler:^(NSError * _Nullable error) {
-        if (error) {
+- (void) sendMessage:(NSString *)message
+{
+    [webSocketTask sendMessage: [[NSURLSessionWebSocketMessage alloc] initWithString:message] completionHandler:^(NSError * _Nullable error)
+    {
+        if (error) 
+        {
             error_callback();
-        } else {
-            NSLog(@"Sent message: %@", message);
-        }
+        } 
     }];
 }
 
-- (void)receiveMessage {
-    [webSocketTask receiveMessageWithCompletionHandler:^(NSURLSessionWebSocketMessage * _Nullable message, NSError * _Nullable error) {
-        if (error) {
+- (void)receiveMessage
+{
+    [webSocketTask receiveMessageWithCompletionHandler:^(NSURLSessionWebSocketMessage * _Nullable message, NSError * _Nullable error)
+    {
+        if (error) 
+        {
             error_callback();
             
-        } else if (message.type == NSURLSessionWebSocketMessageTypeString) {
+        } else if (message.type == NSURLSessionWebSocketMessageTypeString) 
+        {
             message_callback(message.string);
-            NSLog(@"Received message: %@", message.string);
         }
         [self receiveMessage];
     }];
 }
 
-- (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didOpenWithProtocol:(NSString *)protocol  API_AVAILABLE(ios(13.0)){
+- (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didOpenWithProtocol:(NSString *)protocol  API_AVAILABLE(ios(13.0))
+{
     open_callback();
     // run the loop to receive messages
     [self receiveMessage];
     
 }
 
-- (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didCloseWithCloseCode:(NSInteger)code reason:(NSData *)reason  API_AVAILABLE(ios(13.0)){
+- (void)URLSession:(NSURLSession *)session webSocketTask:(NSURLSessionWebSocketTask *)webSocketTask didCloseWithCloseCode:(NSInteger)code reason:(NSData *)reason  API_AVAILABLE(ios(13.0))
+{
     [self invalidateAndCancel];
 }
 
-- (void)invalidateAndCancel {
+- (void)invalidateAndCancel
+{
     webSocketTask = nil;
     [session invalidateAndCancel];
     session = nil;
