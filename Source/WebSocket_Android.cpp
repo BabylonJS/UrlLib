64ccfac8aa19696340511dfb12f8fdd445e81cb2
Liyaan Maskati <liyaanmaskati@gmail.com>
2023-04-18 14:03:11 -0700

    use unique ptr instead of raw pointer

--- Source/WebSocket_Android.cpp
+++ Source/WebSocket_Android.cpp
@@ -14,33 +14,18 @@ namespace UrlLib
     class WebSocket::WSImpl : public WSImplBase
     {
     public:
-        ~WSImpl()
-        {
-            delete m_webSocket;
-        }
-
         void Open(std::string url,
             std::function<void()> onopen,
             std::function<void()> onclose,
             std::function<void(std::string)> onmessage,
             std::function<void()> onerror)
         {
-            open_callback = onopen;
-            close_callback = onclose;
-            message_callback = onmessage;
-            error_callback = onerror;
-
             m_url = url;
-            m_webSocket = new WebSocketClient(url, open_callback, close_callback, message_callback, error_callback);
+            m_webSocket = std::make_unique<WebSocketClient>(url, onopen, onclose, onmessage, onerror);
         }
 
         void Send(std::string message)
         {
-            if (!m_webSocket)
-            {
-                throw std::runtime_error{"WebSocket is not initialized"};
-            }
-
             m_webSocket->Send(message);
         }
 
@@ -49,12 +34,8 @@ namespace UrlLib
             m_webSocket->Close();
         }
 
-    private:     
-        WebSocketClient* m_webSocket;
-        std::function<void()> open_callback;
-        std::function<void()> close_callback;
-        std::function<void(std::string)> message_callback;
-        std::function<void()> error_callback;
+    private:
+        std::unique_ptr<WebSocketClient> m_webSocket{};
     };
 }
 
