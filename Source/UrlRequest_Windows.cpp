c744357c4e409fa64f74fd740ee579a8d881b275
Andy Fillebrown <afillebrown@microsoft.com>
2023-10-02 17:15:03 -0400

    Improve file response buffer check

--- Source/UrlRequest_Windows.cpp
+++ Source/UrlRequest_Windows.cpp
@@ -168,7 +168,7 @@ namespace UrlLib
 
         gsl::span<const std::byte> ResponseBuffer() const
         {
-            if (m_fileResponseBuffer.size() > 0)
+            if (!m_fileResponseBuffer.empty())
             {
                 return {(std::byte*)m_fileResponseBuffer.data(), gsl::narrow_cast<std::size_t>(m_fileResponseBuffer.size())};
             }
